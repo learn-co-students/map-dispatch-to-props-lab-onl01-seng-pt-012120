7f9a67f888975c3f7bfd44a6ac4db2713e03019d
d-otis <dan@dan-foley.com>
2020-10-09 12:55:55 -0400

    Done.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -6,7 +6,7 @@ class Restaurants extends Component {
   render() {
 
     let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
-
+    console.log(restaurants)
     return (
       <div>
         <ul>
