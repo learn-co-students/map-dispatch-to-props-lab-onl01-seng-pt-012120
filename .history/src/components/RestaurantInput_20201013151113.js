df30688857242d041d3714a50661ed92bb7ca970
Charlie Pelton <charliejp0311@gmail.com>
2020-10-14 09:00:13 -0500

    Done.

---
+++ .history/src/components/RestaurantInput_20201013151113.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { addRestaurant } from '../actions/restaurants';
+import { connect } from 'react-redux';
+
+export class RestaurantInput extends Component {
+
+  state = {
+    name: '',
+    location: ''
+  }
+
+  handleOnNameChange = event => {
+    this.setState({
+      name: event.target.value
+    });
+  }
+
+  handleOnLocationChange = event => {
+    this.setState({
+      location: event.target.value
+    });
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault();
+    // add missing code
+    // console.log(this.state)
+    this.props.addRestaurant();
+  }
+
+  render() {
+    return(
+      <form onSubmit={(event) => this.handleOnSubmit(event)}>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnNameChange(event)}
+            id="name"
+            placeholder="restaurant name" />
+        </p>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnLocationChange(event)}
+            id="location"
+            placeholder="location" />
+        </p>
+        <input type="submit" />
+      </form>
+    );
+  }
+};
+
+
+//connect this component by wrapping RestaurantInput below
+
+const mapDispatchToProps = dispatch =>{
+  return {
+    addRestaurant: ()=>{
+      dispatch(addRestaurant())
+    }
+  }
+}
+
+export default connect(mapDispatchToProps)(RestaurantInput)
+ 
